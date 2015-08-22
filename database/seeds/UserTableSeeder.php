3dbaf1a6e1b849d6c826dfa82a0d20547a1cf1eb
Deyves <deyvescarvalho@gmail.com>
2015-08-22 13:27:04 -0300

    autenticacao token

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,13 @@ class UserTableSeeder extends Seeder
     public function run()
     {
 //        \App\Entities\Client::truncate();
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Deyves',
+            'email' => 'deysasves@gmail.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
+
         factory(\App\Entities\User::class, 10)->create();
     }
 }
